59da52555a77542d946412e38cb508f89c764273
Mateus Pereira <mateusrobr@gmail.com>
2025-04-05 17:32:37 -0300

    use of slices and rand module to create a random format to greet the user

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,13 +3,23 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(RandomFormat(), name)
 
 	return message, nil
+}
+
+func RandomFormat() string {
+	formats := []string{"Hello, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
